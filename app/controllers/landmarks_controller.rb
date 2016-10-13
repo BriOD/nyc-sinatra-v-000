6c2d303ddbff05f3fd7c130936f01e00d58d7dad
BriOD <briodonoghue@gmail.com>
2016-10-13 15:09:36 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,43 @@
 class LandmarksController < ApplicationController
+  get '/' do
+    redirect "/landmarks"
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+
+    redirect '/landmarks'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params["landmark"]["name"],
+      year_completed: params["landmark"]["year_completed"])
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 
 end
